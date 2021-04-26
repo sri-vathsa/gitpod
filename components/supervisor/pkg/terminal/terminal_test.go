16c9f86a98922416b1485ebe97083b5aef4f585e
sri-vathsa <vathsa.vemula@codenation.co.in>
2021-04-26 11:19:10 +0000

    Added terminal tests

--- components/supervisor/pkg/terminal/terminal_test.go
+++ components/supervisor/pkg/terminal/terminal_test.go
@@ -5,70 +5,70 @@
 package terminal
 
 import (
-	// "bytes"
+	"bytes"
 	"context"
 	"io"
 	"os/exec"
 	"strings"
 	"testing"
-	// "time"
+	"time"
 
-	// "github.com/gitpod-io/gitpod/supervisor/api"
-	// "github.com/google/go-cmp/cmp"
+	"github.com/gitpod-io/gitpod/supervisor/api"
+	"github.com/google/go-cmp/cmp"
 	"golang.org/x/sync/errgroup"
 )
 
-// func TestTerminals(t *testing.T) {
-// 	tests := []struct {
-// 		Desc        string
-// 		Stdin       []string
-// 		Expectation func(terminal *Term) string
-// 	}{
-// 		{
-// 			Desc: "recorded output should be equals read output",
-// 			Stdin: []string{
-// 				"echo \"yarn\"",
-// 				"echo \"gp sync-done init\"",
-// 				"echo \"yarn --cwd theia-training watch\"",
-// 				"history",
-// 				"exit",
-// 			},
-// 			Expectation: func(terminal *Term) string {
-// 				return string(terminal.Stdout.recorder.Bytes())
-// 			},
-// 		},
-// 	}
-// 	for _, test := range tests {
-// 		t.Run(test.Desc, func(t *testing.T) {
-// 			terminalService := NewMuxTerminalService(NewMux())
-// 			resp, err := terminalService.Open(context.Background(), &api.OpenTerminalRequest{})
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			terminal, ok := terminalService.Mux.Get(resp.Alias)
-// 			if !ok {
-// 				t.Fatal("no terminal")
-// 			}
-// 			stdoutOutput := bytes.NewBuffer(nil)
+func TestTerminals(t *testing.T) {
+	tests := []struct {
+		Desc        string
+		Stdin       []string
+		Expectation func(terminal *Term) string
+	}{
+		{
+			Desc: "recorded output should be equals read output",
+			Stdin: []string{
+				"echo \"yarn\"",
+				"echo \"gp sync-done init\"",
+				"echo \"yarn --cwd theia-training watch\"",
+				"history",
+				"exit",
+			},
+			Expectation: func(terminal *Term) string {
+				return string(terminal.Stdout.recorder.Bytes())
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Desc, func(t *testing.T) {
+			terminalService := NewMuxTerminalService(NewMux())
+			resp, err := terminalService.Open(context.Background(), &api.OpenTerminalRequest{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			terminal, ok := terminalService.Mux.Get(resp.Alias)
+			if !ok {
+				t.Fatal("no terminal")
+			}
+			stdoutOutput := bytes.NewBuffer(nil)
 
-// 			go func() {
-// 				// give the io.Copy some time to start
-// 				time.Sleep(500 * time.Millisecond)
+			go func() {
+				// give the io.Copy some time to start
+				time.Sleep(500 * time.Millisecond)
 
-// 				for _, stdin := range test.Stdin {
-// 					_, _ = terminal.PTY.Write([]byte(stdin + "\r\n"))
-// 				}
-// 			}()
-// 			_, _ = io.Copy(stdoutOutput, terminal.Stdout.Listen())
+				for _, stdin := range test.Stdin {
+					_, _ = terminal.PTY.Write([]byte(stdin + "\r\n"))
+				}
+			}()
+			_, _ = io.Copy(stdoutOutput, terminal.Stdout.Listen())
 
-// 			expectation := strings.Split(test.Expectation(terminal), "\r\n")
-// 			actual := strings.Split(string(stdoutOutput.Bytes()), "\r\n")
-// 			if diff := cmp.Diff(expectation, actual); diff != "" {
-// 				t.Errorf("unexpected output (-want +got):\n%s", diff)
-// 			}
-// 		})
-// 	}
-// }
+			expectation := strings.Split(test.Expectation(terminal), "\r\n")
+			actual := strings.Split(stdoutOutput.String(), "\r\n")
+			if diff := cmp.Diff(expectation, actual); diff != "" {
+				t.Errorf("unexpected output (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
 
 func TestConcurrent(t *testing.T) {
 	var (
